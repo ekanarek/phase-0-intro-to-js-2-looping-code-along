4548f5ae5822055e7148d365392950d934d3e451
Emily Kanarek <emily.kanarek@powerhrg.com>
2024-02-16 12:48:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const nameList = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return cards;
+}
+
+writeCards(nameList, "surprise");
+
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
